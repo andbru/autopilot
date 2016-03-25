bbb711e436b597689241995083ea365658c5674b
Anders <anders.bruse@gmail.com>
2016-03-25 11:28:30 +0100

    Updates in adc1220

--- adc1220/adc1220.c
+++ adc1220/adc1220.c
@@ -27,6 +27,10 @@ int main() {
 	int adcHandle;
 	//struct timeval t0, t1;
 	
+	wiringPiSetup();
+	pinMode(6, INPUT);
+	pullUpDnControl(6, PUD_OFF);
+	
 	adcHandle = wiringPiSPISetup(0, 1000000);			// Channel 0, speed 1 MHz, get filedescriptor
 	printf("Filedescriptor: %d\n", adcHandle);
 	
@@ -38,7 +42,7 @@ int main() {
 	
 	// Write contents of control registers
 	unsigned char ctrl[5];
-	ctrl[0] = 0x00;
+	ctrl[0] = 0x43;		// WREG cmd, write 4 register starting at 0x00
 	ctrl[1] = 0x80;
 	ctrl[2] = 0x04;
 	ctrl[3] = 0x00;
@@ -46,41 +50,32 @@ int main() {
 	ioc = wiringPiSPIDataRW(0, ctrl, 5);
 	printf("Mode set return should be > -1: %d\n", ioc);
 
-/*
-	// Write to config reg, MSB = 48, LSB = 73 -- write swaps the bytes
-	//int success = wiringPiI2CWriteReg16(adcHandle, 0x01, 0x7348);
-	char buf[3];
-	buf[0] = 0x01;
-	buf[1] = 0x48;
-	buf[2] = 0x73;
-	ioc = write(adcHandle, buf, 3);
-
-	double fres0, fres1;			// Filter variables
-	for(;;) {	
+	// Send start/sync command 0x08
+	unsigned char cmd[2];
+	cmd[0] = 0x08;
+	cmd[1] = 0x00;
+	ioc = wiringPiSPIDataRW(0, cmd, 1);
+	printf("Start/sync command, return should be > -1: %d\n", ioc);
+		
+	// Endless read loop
+	for (;;) {
 	
-		// Read conversion register, swap the bytes and shift right 4 bits
-		//unsigned int iRes = wiringPiI2CReadReg16(adcHandle, 0x00);
-		buf[0] = 0x00;		// Dummy write to set address
-		buf[1] = 0x00;
-		write(adcHandle, buf, 1);
-		unsigned char val[2];
-		read(adcHandle, &val, 2);
-
-		int raw = val[0] * 256 + val[1];	// Combine the two bytes
-		signed int res = raw >> 4;		// Shift the 12 bit value to lower bits
-		if(res & 0x0800) res = res | 0xfffff000; // and fill out with "1"'s when negative 
-		
-		fres1 = res *0.05 + fres0*0.95;		// Digital first order LP-filter
-		fres0 = fres1;
-		
-		printf("%#x   %d  %f    ", res, res, fres1);
-		
-		gettimeofday(&t1, NULL);
-		double dAG = elapsed(t1, t0);
-		t0 = t1;
-		printf(" %f \n ", dAG);
+		// Check DRDY#
+		if (digitalRead(6) == 0) {
+			// Read 3 data bytes
+			unsigned char rData[4];
+			rData[0] = 0x10;		// RDATA cmd
+			rData[1] = 0x00;
+			rData[2] = 0x00;
+			rData[3] = 0x00;
+			ioc = wiringPiSPIDataRW(0, rData, 4);
+			//printf("Mode set return should be > -1: %d\n", ioc);
+			//printf("MRead data = : %#x  %#x  %#x\n", rData[1], rData[2], rData[3]);
+			int res = rData[1] * 256 * 256 + rData[2] * 256 + rData[3];
+			printf("%#x  %d\n", res, res);
+		}
 	}
-*/	
+
 	return 0;
 }
 
