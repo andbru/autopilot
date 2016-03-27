9ffe56c9084d93b91fd2b5279ab8c2309ef78ebf
Anders <anders.bruse@gmail.com>
2016-03-27 15:07:25 +0200

    Rotary encoder included

--- pilot/pilot.c
+++ pilot/pilot.c
@@ -17,6 +17,8 @@
 double elapsed(struct timeval t1, struct timeval t0);
 void initRudder(void);
 int pollRudder(double *angel);
+void initKnob(void);
+int pollKnob(int *mode, double *yawCmd);
 
 // Globals
 
@@ -25,21 +27,30 @@ int main() {
 
 	double rudderIs = 0;
 	struct timeval t0, t1;
-		
+	int mode = 0;
+	double yawCmd = 0;
+	
+	wiringPiSetup();	
 	initRudder();
+	initKnob();
+	
+	mode = 1;
 	
 	// Endless main loop
 	for (;;) {
 	
 		int newAngel = pollRudder(&rudderIs);
 		if(newAngel) {
-			printf("%f  ", rudderIs);
+			printf("%f  %d  %f  ", rudderIs, mode, yawCmd);
 			
 			gettimeofday(&t1, NULL);		// dt = time between iterations
 			double dt = elapsed(t1, t0);
 			t0 = t1;
 			printf("    %f \n ", dt);
 		}
+		
+		int newMode = pollKnob(&mode, &yawCmd);
+		if(newMode) printf("%d  %f\n", mode, yawCmd);
 	}
 
 	return 0;
@@ -60,7 +71,6 @@ void initRudder(void) {
 
 	int adcHandle;
 	
-	wiringPiSetup();
 	pinMode(6, INPUT);
 	pullUpDnControl(6, PUD_OFF);
 	
@@ -99,6 +109,13 @@ int pollRudder(double *angel) {
 	static double fu = 0;				// Filtered voltage
 	static double fk = 0.1;			// Filter constant
 	
+	//  *******************************************************************************************
+	//  Conversion to degrees, y angel in deg, x signal in volt. Straight line y=kx+m. degPerVolt = k. 
+	//  uZeroDeg = voltage at 0 deg => m = - k * uZeroDeg = - degPerVolt * uZeroDeg
+	//  *******************************************************************************************
+	static double degPerVolt = 81.8 ;
+	static double uZeroDeg = 1.30;
+	
 	// Check DRDY#
 	if (digitalRead(6) == 0) {
 		// Read 3 data bytes
@@ -114,8 +131,56 @@ int pollRudder(double *angel) {
 		
 		fu = u * fk + (1.0 - fk) * fu; 		// Digital filter
 		
-		
-		*angel = fu;
+		*angel = degPerVolt *fu - degPerVolt * uZeroDeg; 	// Conversion to deg
+
+		return 1;
+	}
+	
+	return 0;
+}
+
+
+void initKnob(void) {
+
+	pinMode(0, INPUT);
+	pinMode(2, INPUT);
+	pinMode(3, INPUT);
+	pullUpDnControl(0, PUD_UP);
+	pullUpDnControl(2, PUD_UP);
+	pullUpDnControl(3, PUD_UP);
+
+}
+
+
+int pollKnob(int *mode, double *yawCmd) {
+	
+	static int clk0 = 1;
+	static int clk1 = 1;
+	static int dt1 = 1;
+	static int sw = 1;
+	static int swStart = 1000;		// no of consecutive low to avoid switch bumps
+	static int swCount = 1000;
+	static double degPerClick = 0.5;	// Gearing
+	
+	if(*mode == 0) return 0;		// Still starting up
+	
+	dt1 = digitalRead(2);
+	clk1 = digitalRead(3);
+	sw = digitalRead(0);
+	
+	int upDown = 0;
+	if(clk1 < clk0) {		// falling
+		if(dt1 == 1) upDown++; else upDown--;
+	}
+	if(clk1 > clk0) {		// raising
+		if(dt1 == 1) upDown--; else upDown++;
+	}
+	clk0 = clk1;
+	*yawCmd += upDown * degPerClick;
+	
+	if(sw == 0) swCount--; else 	swCount = swStart;	
+	if(swCount == 0) {
+		if(*mode == 1) *mode = 2; else *mode = 1;		// Toggle mode
 		return 1;
 	}
 	
